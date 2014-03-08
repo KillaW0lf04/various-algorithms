3c408f1f395e3bb9701a97cf64690f10f2af3902
Michael Aquilina <michaelaquilina@gmail.com>
2014-03-08 23:02:22 +0000

    Use swap from the std library

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,24 +1,19 @@
+#include <algorithm>
+
 void quicksort(int *buffer, int start, int end) {
     if( start < end) {
         int mid = buffer[end];
-        int dummy;
 
         int divider = start;
         for(int i=start; i < end; i++) {
             if(buffer[i] <= mid)
             {
-                // Swap
-                dummy = buffer[divider];
-                buffer[divider] = buffer[i];
-                buffer[i] = dummy;
-
+                std::swap(buffer[i], buffer[divider]);
                 divider += 1;
             }
         }
 
-        dummy = buffer[end];
-        buffer[end] = buffer[divider];
-        buffer[divider] = dummy;
+        std::swap(buffer[divider], buffer[end]);
 
         // Recursively sort for the rest
         quicksort(buffer, start, divider - 1);
