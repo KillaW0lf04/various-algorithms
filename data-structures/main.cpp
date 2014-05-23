37dab9ded5fdbbcf0767395dc9be26e03c9cfb88
Michael Aquilina <michaelaquilina@gmail.com>
2014-05-23 11:45:42 +0100

    Change tests to use TEST FIXTURES instead for re-usability

--- data-structures/main.cpp
+++ data-structures/main.cpp
@@ -7,52 +7,49 @@
 
 using namespace std;
 
-TEST(HashtableTest, AddGetTest) {
-    Hashtable *table = new Hashtable(6);
+class HashtableTest  : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        table = new Hashtable(6);
+
+        table->add("michael", &age1);
+        table->add("christina", &age2);
+        table->add("lorraine", &age3);
+        table->add("stephanie", &age4);
+        table->add("bruce", &age5);
+    }
 
     int age1 = 25;
     int age2 = 22;
     int age3 = 54;
     int age4 = 24;
     int age5 = 4;
+    Hashtable *table;
+};
 
-    table->add("michael", &age1);
-    table->add("christina", &age2);
-    table->add("lorraine", &age3);
-    table->add("stephanie", &age4);
-    table->add("bruce", &age5);
+TEST_F(HashtableTest, SizeTest) {
+    EXPECT_EQ(5, table->size());
+}
 
+TEST_F(HashtableTest, GetTest) {
     // Ensure added items are found
     EXPECT_EQ(age1, *((int *) table->get("michael")));
     EXPECT_EQ(age2, *((int *) table->get("christina")));
     EXPECT_EQ(age3, *((int *) table->get("lorraine")));
     EXPECT_EQ(age4, *((int *) table->get("stephanie")));
     EXPECT_EQ(age5, *((int *) table->get("bruce")));
+}
 
-    // Size check
-    EXPECT_EQ(5, table->size());
-
-    // Case Sensitive check
-    EXPECT_EQ(nullptr, table->get("CHRISTINA"));
-
-    // Non-Existant check
+TEST_F(HashtableTest, DoesNotExistTest) {
     EXPECT_EQ(nullptr, table->get("idontexist"));
+}
 
-    // Removal Test
+TEST_F(HashtableTest, CaseSensitiveTest) {
+    EXPECT_EQ(nullptr, table->get("CHRISTINA"));
+}
+
+TEST_F(HashtableTest, RemoveTest) {
+    EXPECT_NE(nullptr, table->get("michael"));
     table->remove("michael");
     EXPECT_EQ(nullptr, table->get("michael"));
-    EXPECT_EQ(4, table->size());
-
-    // Remove non-existant field
-    table->remove("idontexist");
-
-    // Remove all
-    table->remove("christina");
-    table->remove("lorraine");
-    table->remove("stephanie");
-    table->remove("bruce");
-
-    EXPECT_EQ(0, table->size());
-
-    delete table;
 }
